5d601b5ab70ba1ba8a0edfb97136a1447c7cc141
Manon Kim <kim.manon2@gmail.com>
2024-05-23 22:44:28 +0900

    [refactor] code splitting (lazy, suspense)

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,61 @@
+import { lazy, Suspense } from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
-import HotelListPage from '@pages/HotelList'
-import TestPage from '@pages/Test'
-import HotelPage from './pages/Hotel'
-import useLoadKakao from './hooks/useLoadKakao'
-import Mypage from './pages/My'
-import SigninPage from './pages/Signin'
-import AuthGuard from './components/auth/AuthGuard'
+
 import Navbar from './components/shared/Navbar'
+import AuthGuard from './components/auth/AuthGuard'
+import PrivateRoute from './components/auth/PrivateRoute'
+import useLoadKakao from './hooks/useLoadKakao'
+
+const TestPage = lazy(() => import('@pages/Test'))
+const HotelListPage = lazy(() => import('@pages/HotelList'))
+const HotelPage = lazy(() => import('@pages/Hotel'))
+const Mypage = lazy(() => import('@pages/My'))
+const SigninPage = lazy(() => import('@pages/Signin'))
+const SettingsPage = lazy(() => import('@/pages/Setting'))
+const LikePage = lazy(() => import('@/pages/Setting/like'))
 
 function App() {
   useLoadKakao()
 
   return (
-    <BrowserRouter>
-      <AuthGuard>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<HotelListPage />}></Route>
-          <Route path="/test" element={<TestPage />}></Route>
-          <Route path="/mypage" element={<Mypage />}></Route>
-          <Route path="/signin" element={<SigninPage />}></Route>
-          <Route path="/hotel/:id" element={<HotelPage />}></Route>
-        </Routes>
-      </AuthGuard>
-    </BrowserRouter>
+    <Suspense fallback={<></>}>
+      <BrowserRouter>
+        <AuthGuard>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<HotelListPage />}></Route>
+            <Route path="/test" element={<TestPage />}></Route>
+            <Route
+              path="/mypage"
+              element={
+                <PrivateRoute>
+                  <Mypage />
+                </PrivateRoute>
+              }
+            ></Route>
+            <Route path="/signin" element={<SigninPage />}></Route>
+            <Route path="/hotel/:id" element={<HotelPage />}></Route>
+            <Route
+              path="/settings"
+              element={
+                <PrivateRoute>
+                  <SettingsPage />
+                </PrivateRoute>
+              }
+            ></Route>
+            <Route
+              path="/settings/like"
+              element={
+                <PrivateRoute>
+                  <LikePage />
+                </PrivateRoute>
+              }
+            />
+          </Routes>
+        </AuthGuard>
+      </BrowserRouter>
+    </Suspense>
   )
 }
 
